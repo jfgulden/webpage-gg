6db74e4cf587ef144068cee209c0609e56d205c3
Juan Francisco <guldenjf@gmail.com>
2024-04-05 11:28:28 -0300

    contact sect

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+
+    if (!nombre || !email) {
+        document.getElementById('mensajeAdvertencia').style.display = 'block';
+    } else {
+        document.getElementById('mensajeAdvertencia').style.display = 'none';
+        document.getElementById('miFormulario').submit();
+    }
+}
